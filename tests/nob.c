825040a2b614db7e06ba65b3acdbda0e729958c4
luxluth <delphin.blehoussi93@gmail.com>
2025-09-02 10:36:49 +0200

    restructuring tests

--- tests/nob.c
+++ tests/nob.c
@@ -2,7 +2,7 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
-#define BUILD_DIR "build/"
+#define BUILD_DIR "../build/"
 
 int main(int argc, char **argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
@@ -10,8 +10,8 @@ int main(int argc, char **argv) {
   mkdir_if_not_exists(BUILD_DIR);
 
   Cmd cmd = {0};
-  cmd_append(&cmd, "clang", "-Wall", "-Wextra", "-O3", "-o", BUILD_DIR "test",
-             "test.c");
+  cmd_append(&cmd, "clang", "-Wall", "-Wextra", "-O3", "-I..", "-o",
+             BUILD_DIR "test", "test.c");
   if (!cmd_run(&cmd))
     return 1;
 
