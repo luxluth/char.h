5efd3836a9d671f546811b67ad05e3e9a93d9e31
luxluth <delphin.blehoussi93@gmail.com>
2025-09-02 10:28:02 +0200

    License

--- char.h
+++ char.h
@@ -364,3 +364,31 @@ CHARDEF bool char_string_iter_next(Char_StringIter *it, Char_Char *out_char) {
 #endif // CHAR_STRIP_PREFIX
 
 #endif // CHAR_STRIP_PREFIX_GUARD_
+
+/* ----------------------------------------------------------------------------
+MIT - licence
+
+Copyright (c) 2025 Delphin Blehoussi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+Great Thanks to the Alexey Kutepov
+-----------------------------------------------------------------------------*/
